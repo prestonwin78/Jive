b6a9c07f4827a08f5b682b4634b12464041a6086
prestonwin78 <prestonwin78@gmail.com>
2020-08-17 17:11:20 -0500

    Move login to function

--- public/Calendar/input.js
+++ public/Calendar/input.js
@@ -1,4 +1,6 @@
 
+import {displayLoginPopup} from './loginpopup.js';
+
 let userId = "";
 let signOutPressed = false;
 
@@ -19,45 +21,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log("user is signed out");
     
     if(!signOutPressed){
-      //display overlay
-      let overlay = document.getElementById("overlay");
-      overlay.style.display = "block";
-     }
-
-    //Add functionality to cancel and sign-in buttons
-    let cancelButton = document.getElementById("cancel-button");
-    let submitButton = document.getElementById("signin-button");
-
-    cancelButton.addEventListener("click", function(e){
-        e.preventDefault();
-        //redirect to home page if user cancels
-        window.location.href = "../index.html";
-    });
-
-    submitButton.addEventListener("click", function(e){
-      e.preventDefault();
-
-      //get user email and password input
-      let email = document.getElementById("usernameinput").value.toString();
-      let password = document.getElementById("passwordinput").value;
-
-      //authenticate with firebase
-      firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-      (user) => {
-          if(user){
-              //if user successfully signs in, remove overlay
-              //and proceed to main logic
-              let overlay = document.getElementById("overlay");
-              overlay.style.display = "none";
-              afterInit();  //main logic
-            } 
-          }
-      ).catch(function(e){
-        //display error message
-        document.getElementById("error-message").style.display = "inline-block";
-        });
-    });
+      displayLoginPopup();
+    }
   }
 });
 
---
+++ public/Calendar/loginpopup.js
@@ -0,0 +1,40 @@
+ export function displayLoginPopup(){
+    //display overlay
+    let overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+
+    //Add functionality to cancel and sign-in buttons
+    let cancelButton = document.getElementById("cancel-button");
+    let submitButton = document.getElementById("signin-button");
+
+    cancelButton.addEventListener("click", function(e){
+        e.preventDefault();
+        //redirect to home page
+        window.location.href = "../index.html";
+    });
+
+    submitButton.addEventListener("click", function(e){
+        e.preventDefault(); //don't submit form
+
+        //get user email and password input
+        let email = document.getElementById("usernameinput").value.toString();
+        let password = document.getElementById("passwordinput").value;
+
+        //authenticate with firebase
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(
+        (user) => {
+            if(user){
+                //if user successfully signs in, remove overlay
+                //and proceed to main logic
+                let overlay = document.getElementById("overlay");
+                overlay.style.display = "none";
+                afterInit();  //main logic
+                } 
+            }
+        ).catch(function(e){
+            //display error message
+            document.getElementById("error-message").style.display = "inline-block";
+        });
+    });
+}
