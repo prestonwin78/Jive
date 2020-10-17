812ab4477131d57fd376bff7fd54d637a04ce7e0
prestonwin78 <prestonwin78@gmail.com>
2020-10-17 14:55:28 -0500

    Fix plus styling

---
+++ public/Calendar/pagechanges.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  console.log("doc ready");
+  $(".daycard")
+    .on('mouseenter', (event) => {
+      console.log("hovering!");
+      $(event.currentTarget).find(".delete-container").css({
+        visibility: "visible"
+      });
+      $(event.currentTarget).find(".plus-container").css({
+        visibility: "visible"
+      });
+    })
+    .on('mouseleave', (event) => {
+      $(event.currentTarget).find(".delete-container").css({
+        visibility: "hidden"
+      });
+      $(event.currentTarget).find(".plus-container").css({
+        visibility: "hidden"
+      });
+    });
+});
