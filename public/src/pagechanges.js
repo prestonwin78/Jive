77e178584d9268444b0dae19681f8b81556884e4
prestonwin78 <prestonwin78@gmail.com>
2020-10-22 14:07:21 -0500

    Add startup animation

--- public/src/pagechanges.js
+++ public/src/pagechanges.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
-  console.log("doc ready");
+  initializeStyles()
+  .then(() => {
+    animateRows();
+  })
   $(".daycard")
     .on('mouseenter', (event) => {
       $(event.currentTarget).find(".delete-container").css({
@@ -18,3 +21,29 @@ $(document).ready(() => {
       });
     });
 });
+
+async function initializeStyles(){
+  $("#day-row-1").css({
+    marginTop: "50px",
+    opacity: "0%",
+    visibility: "visible"
+  });
+  $("#day-row-2").css({
+    marginTop: "50px",
+    opacity: "0%",
+    visibility: "visible"
+  });
+}
+
+function animateRows() {
+  $("#day-row-1").show();
+  $("#day-row-2").show();
+  $("#day-row-1").animate({
+    marginTop: "0px",
+    opacity: "100%"
+  }, 2000);
+  $("#day-row-2").animate({
+    marginTop: "0px",
+    opacity: "100%"
+  }, 2000);
+}
